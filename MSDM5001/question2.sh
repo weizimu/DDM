c26e970955b483189d84c188fab418d69950931e
weizimu <386945989@qq.com>
2020-10-19 15:25:57 +0800

    update question2.sh

--- MSDM5001/question2.sh
+++ MSDM5001/question2.sh
@@ -5,6 +5,6 @@ do
 	cd DDM$a
 	touch time_till_now.txt
 	echo "nanoseconds since 1970-01-01 00:00:00 UTC:">time_till_now.txt
-	date>>time_till_now.txt
+	echo "<`date +%s`>">>time_till_now.txt
 	cd ../
 done
